63c8edc17d5d68bbb6288a96802fbd17ac3c1c4e
Athar M <93383101+KingPin-1@users.noreply.github.com>
2022-11-21 22:29:15 +0530

    added unionBySize

--- DisjointSetUnion.java
+++ DisjointSetUnion.java
@@ -3,10 +3,12 @@ import java.util.*;
 class UnionFind {
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
     public UnionFind(int n){        // Constructor
         for( int i = 0 ; i <= n ; i++ ) {
             rank.add(0);
             parent.add(i);
+            size.add(1);
         }
     }
     public int findParent(int u){
@@ -33,25 +35,58 @@ class UnionFind {
             rank.set(ult_u,ult_rank+1);
         }
     }
+    
+    public void unionBySize(int u , int v){
+        int ult_u = findParent(u);
+        int ult_v = findParent(v);
+        if( ult_v == ult_u )
+            return;
+        if( size.get(ult_v) < size.get(ult_u) ) {
+            parent.set(ult_v, ult_u);
+            size.set(ult_u,size.get(ult_u) + size.get(ult_v));
+        }
+        else{
+            parent.set(ult_u, ult_v);
+            size.set(ult_v,size.get(ult_u) + size.get(ult_v));
+        }
+    }
 }
 
 public class DisjointSetUnion {
     public static void main(String[] args) {
         UnionFind ds = new UnionFind(7);
-        ds.unionByRank(1,2);
-        ds.unionByRank(2,3);
-        ds.unionByRank(4,5);
-        ds.unionByRank(6,7);
-        ds.unionByRank(5,6);
+        // main for bySize
+        ds.unionBySize(1,2);
+        ds.unionBySize(2,3);
+        ds.unionBySize(4,5);
+        ds.unionBySize(6,7);
+        ds.unionBySize(5,6);
         if( ds.findParent(3) == ds.findParent(7) )
             System.out.println("Same Component");
         else
             System.out.println("Not same");
-            ds.unionByRank(3,7);
+            ds.unionBySize(3,7);
         if( ds.findParent(3) == ds.findParent(7) )
             System.out.println("Same Component");
         else
             System.out.println("Not same");
-        System.out.println(ds.parent +" -> " + ds.rank);
+        System.out.println(ds.parent +" -> " + ds.size);
+
+        // main for byRank
+        // ds.unionByRank(1,2);
+        // ds.unionByRank(2,3);
+        // ds.unionByRank(4,5);
+        // ds.unionByRank(6,7);
+        // ds.unionByRank(5,6);
+        // if( ds.findParent(3) == ds.findParent(7) )
+        //     System.out.println("Same Component");
+        // else
+        //     System.out.println("Not same");
+        //     ds.unionByRank(3,7);
+        // if( ds.findParent(3) == ds.findParent(7) )
+        //     System.out.println("Same Component");
+        // else
+        //     System.out.println("Not same");
+        // System.out.println(ds.parent +" -> " + ds.rank);
     }
 }
